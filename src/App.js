815f6e71c68b9a64c92fa4aea3722865a1c5de8b
Brian Louie <brianalouie@gmail.com>
2019-01-30 20:58:24 -0800

    working with hooks and connect, no middleware yet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Store, Provider} from './store';
+import Controls from './Controls';
+import Message from './Message';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Provider>
+        <Controls />
+        {/*with connect*/}
+        <Message />
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
